239a9e087699dcb39025ce66e7548e025d387359
Anxhela Decka <adecka@helius-software.com>
2018-08-17 14:43:14 +0200

    Angular router basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,12 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -27,21 +33,29 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
-      BrowserModule,
+    BrowserModule,
 
-      MatListModule,
+    MatListModule,
 
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
 
-      BrowserAnimationsModule,
-      MatToolbarModule,
-      FlexLayoutModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+
+    AppRoutingModule
+
   ],
+
   providers: [ DishService ],
   bootstrap: [AppComponent]
 })
